fd2964aacb6d6593ff0bf8aaf9413820596c7725
MilkFrog <jeff.puree@gmail.com>
2023-09-26 04:45:34 +0200

    setup project with figlet

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -1,8 +1,15 @@
+import figlet from "figlet";
+
 console.log("Hello via Bun!");
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response("Bun!");
+    // log the request
+    console.log(req.method, req.url, req.destination);
+
+    // create a header object
+    const body = figlet.textSync("Bun!");
+    return new Response(body);
   },
 });
 
